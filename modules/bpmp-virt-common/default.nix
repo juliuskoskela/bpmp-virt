63d2fe375b9b2e745132ecb4812e2bffab950154
Julius Koskela <me@juliuskoskela.dev>
2023-11-06 13:32:49 +0200

    Add bpmp-virt-common module
    Signed-off-by: Julius Koskela <me@juliuskoskela.dev>

---
+++ modules/bpmp-virt-common/default.nix
@@ -0,0 +1,29 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
+  pkgs.overlays = [import ../../overlays/qemu];
+
+  boot.kernelPatches = [
+    {
+      name = "Added Configurations to Support Vda";
+      patch = ./patches/added-configurations-to-support-vda.patch;
+    }
+    {
+      name = "Vfio_platform Reset Required False";
+      patch = ./pacthes/vfio_platform-reset-required-false.patch;
+    }
+    {
+      name = "Bpmp Support Virtualization";
+      patch = ./pacthes/bpmp-support-bpmp-virt.patch;
+    }
+    {
+      name = "Bpmp Virt Drivers";
+      patch = ./patches/bpmp-virt-drivers.patch;
+    }
+  ];
+
+  boot.kernelParams = ["vfio_iommu_type1.allow_unsafe_interrupts=1"];
+}
