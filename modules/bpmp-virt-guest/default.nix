8dfed220a9719b4fab10a41ace45273153dd12c9
Julius Koskela <me@juliuskoskela.dev>
2023-11-06 13:33:17 +0200

    Add bpmp-virt-guest module
    Signed-off-by: Julius Koskela <me@juliuskoskela.dev>

--- modules/bpmp-virt-guest/default.nix
+++ modules/bpmp-virt-guest/default.nix
@@ -2,42 +2,22 @@
   config,
   lib,
   pkgs,
-  system,
-  microvm,
   ...
-}: let
-  bpmpDtb = ./uarta-qemu-8.1.0.dtb;
-in {
-  uartaVm = microvm.nixosModules.microvm {
-    hypervisor = "qemu";
-    machine = "virt,accel=kvm";
-    cpu = "host";
-    memorySize = "1G";
-    rebootOnExit = false;
-    networks = [
-      {
-        type = "user";
-        hostForwarding = [
-          {
-            protocol = "tcp";
-            hostPort = 2222;
-            guestPort = 22;
-          }
-        ];
-      }
-      {
-        type = "nic";
-      }
-    ];
-    devices = [
-      {
-        type = "vfio-platform";
-        options = "host=3100000.serial";
-      }
-    ];
-    extraArgs = [
-      "-dtb ${bpmpDtb}"
-      "-append \"rootwait root=/dev/vda console=ttyAMA0\""
-    ];
-  };
+}: {
+  imports = [./bpmp-virt-common];
+
+  boot.kernelPatches = [
+    {
+      name = "Bpmp Guest Proxy Dts";
+      patch = ./patches/bpmp-guest-proxy-dts.patch;
+    }
+    {
+      name = "BPMP virt enable guest";
+      patch = null;
+      extraStructuredConfig = with lib.kernel; {
+        VFIO_PLATFORM = yes;
+        TEGRA_BPMP_GUEST_PROXY = yes;
+      };
+    }
+  ];
 }
